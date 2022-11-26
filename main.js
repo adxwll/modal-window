f2ca3f0421f07d7cd50bb4bb292ff87f992a12fe
adxwll <adeliya.ibragimovva08@gmail.com>
2022-11-26 17:06:04 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+let btn = document.querySelector('.btn');
+let div = document.querySelector('.modal');
+let close = document.querySelector('.close');
+
+btn.addEventListener('click', () => {
+    modal.style.display = "block";
+})
+close.addEventListener('click', () => {
+    modal.style.display = "none"
+})
+
+
+let arr = [0,0,1,2,3];
+if( i = 0; i > arr.length; i++){
+ arr = arr.slic
+}
+     console.log(arr)
